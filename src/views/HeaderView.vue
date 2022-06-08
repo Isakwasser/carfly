1623bf96758a4b61835ff49f39670416a43bf06d
Isakwasser <wasser18@bk.ru>
2022-06-08 13:29:25 +0300

    Add header/footer

---
+++ src/views/HeaderView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="Header">
+        <div class="container py-5">
+            <div class="row text-center">
+                <div class="col-12 col-md-8 d-md-flex justify-content-center align-items-center">
+                    <div class="mx-4">Услуги</div>
+                    <div class="mx-4">О парковке</div>
+                    <div class="mx-4"> Где мы?</div>
+                    <div class="mx-4">Камеры</div>
+                </div>
+                <div class="col-12 col-md-4 d-md-flex flex-column flex-lg-row justify-content-around align-items-center">
+                    <div class="logo logo_whatsapp">+7 (921) 000-00-00</div>
+                    <div>8 (800) 555-35-35</div>
+                </div>
+            </div>
+        </div>
+        <div class="Header__postline"></div>
+    </div>
+</template>
+
+<script src="../controllers/HeaderController.js"></script>
+<style lang="scss" src="../styles/_header.scss"></style>
