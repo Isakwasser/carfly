d79d5025ab00c955ac91a6479015c9d16fc15dd2
Isakwasser <wasser18@bk.ru>
2022-06-09 14:16:41 +0300

    Fix footer. add transition

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -2,17 +2,20 @@
     <div class="Footer pt-2">
         <div class="container pb-1">
             <div class="d-flex py-5">
-                <div class="ms-auto">
-                    <a href="https://wa.me/79210000000" target="_blank" class="Footer__contact logo_whatsapp">
+                <div class="ms-auto d-flex">
+                    <a href="https://wa.me/79210000000" target="_blank"
+                        class="Footer__contact d-flex align-items-center">
+                        <img src="../assets/WhatsApp_light.svg" class="img-fluid me-3" alt="">
                         +7(921)000-00-00
                     </a>
                     <a href="tel:88005553535" class="Footer__contact ms-5">Тел. 8 (800) 555-35-35</a>
                 </div>
             </div>
             <div class="Footer__line"></div>
-            <div class="d-flex justify-content-between py-3">
+            <div class="d-flex justify-content-between py-3 Footer__bottom">
                 <div>© Carfly 2020</div>
-                <div>Политика конфидициальности</div>
+                <a href="https://yandex.ru/images/search?from=tabbar&text=заглушка" target="_blank">Политика
+                    конфидициальности</a>
             </div>
         </div>
     </div>
