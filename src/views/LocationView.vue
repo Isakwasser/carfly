9cd7d3fd905089c5e92042e330d55c12d6fe9b91
Isakwasser <wasser18@bk.ru>
2022-06-09 15:13:30 +0300

    Adaptive Location

--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -2,25 +2,26 @@
     <div class="Location position-relative pb-5" id="location">
         <div class="container pt-5">
             <div class="row pt-5">
-                <div class="col-md-5">
+                <div class="col-md-5 mb-4 mb-md-0">
                     <h2 class="mb-4">Где мы находимся</h2>
                     <div class="mb-5">
-                        <h3 class="mb-2">Адрес</h3>
-                        <div class="Location__info">Пулковское ш., д. 41, лит. ЗИ, Санкт-Петербург</div>
+                        <h3 class="mb-2 extra32">Адрес</h3>
+                        <div class="Location__info semi24-32">Пулковское ш., д. 41, лит. ЗИ, Санкт-Петербург</div>
                     </div>
                     <div class="mb-5">
-                        <h3 class="mb-2">Координаты</h3>
-                        <div class="Location__info">55.58804713; 37.21705019</div>
+                        <h3 class="mb-2 extra32">Координаты</h3>
+                        <div class="Location__info semi24-32">55.58804713; 37.21705019</div>
                     </div>
                     <div class="Location__contacts mb-5">
                         <div class="mb-3">
-                            <a href="https://wa.me/79210000000" target="_blank" class="d-flex align-items-center">
+                            <a href="https://wa.me/79210000000" target="_blank"
+                                class="d-flex align-items-center semi24-32">
                                 <img src="../assets/WhatsApp.svg" class="img-fluid me-3" alt="">
                                 +7(921)000-00-00
                             </a>
                         </div>
                         <div>
-                            <a href="tel:88005553535" class="d-flex align-items-center">
+                            <a href="tel:88005553535" class="d-flex align-items-center semi24-32">
                                 <img src="../assets/phone-call.svg" class="img-fluid me-3" alt="">
                                 Тел. 8 (800) 555-35-35</a>
                         </div>
@@ -28,7 +29,7 @@
                     <a href="#" class="Location__action">Найти в Навигаторе</a>
                 </div>
                 <div class="col-md-7">
-                    <div class="h-100 w-100 Location__map">
+                    <div class="h-100 w-100 Location__map text-button">
 
                     </div>
                 </div>
