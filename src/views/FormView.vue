6e5a15adc1bd17f01c0ae5bf9b80f211b68733a2
Isakwasser <wasser18@bk.ru>
2022-06-09 14:48:46 +0300

    Adaptive Form

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -6,16 +6,16 @@
                     <h2 class="pb-5 mb-4">Парковка в&nbsp;Пулково с&nbsp;трансфером в&nbsp;аэропорт</h2>
                     <div class="Form__mini d-flex">
                         <div class="py-4 pe-4">
-                            <div class="Form__duration">1-2 дня</div>
-                            <div class="Form__price">350 рублей</div>
+                            <div class="Form__duration semi24-32">1-2 дня</div>
+                            <div class="Form__price extra24-32">350 рублей</div>
                         </div>
                         <div class="py-4 px-4">
-                            <div class="Form__duration">3-7 дней</div>
-                            <div class="Form__price">250 рублей</div>
+                            <div class="Form__duration semi24-32">3-7 дней</div>
+                            <div class="Form__price extra24-32">250 рублей</div>
                         </div>
                         <div class="py-4 ps-4">
-                            <div class="Form__duration">7-14 дней</div>
-                            <div class="Form__price">200 рублей</div>
+                            <div class="Form__duration semi24-32">7-14 дней</div>
+                            <div class="Form__price extra24-32">200 рублей</div>
                         </div>
                     </div>
                 </div>
@@ -23,8 +23,8 @@
                     <img src="../assets/form_img.svg" alt="" class="img-fluid mt-auto Form__usersImage">
                 </div>
             </div>
-            <form action="#" @submit="submit" class="row pb-4">
-                <div class="col-md-4">
+            <form action="#" @submit="submit" class="row pb-4 semi16-24">
+                <div class="col-md-6 col-lg-4 mb-2">
                     <label for="inDate">Дата и время въезда (вылет - 3 часа)</label>
                     <div class="input">
                         <input required type="date" placeholder="ДД/ММ/ГГ" v-model="startDate">
@@ -32,7 +32,7 @@
                         <div class="dividerBlock"></div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6 col-lg-4">
                     <label for="outDate">Дата и время выезда (прилет + 1 час)</label>
                     <div class="input">
                         <input required type="date" placeholder="ДД/ММ/ГГ" v-model="endDate">
@@ -40,9 +40,9 @@
                         <div class="dividerBlock"></div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-lg-4">
                     <label for="s">&nbsp;</label>
-                    <div class="">
+                    <div class="text-button">
                         <button class="Form__success w-100">Найти место</button>
                     </div>
                 </div>
